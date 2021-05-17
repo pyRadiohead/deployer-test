4226e109c36fe4cbb44ec09b8bccfd0e313d550c
pyRadiohead <ethingwillbefine@gmail.com>
2021-05-17 21:58:38 +0300

    testing push

--- deploy.php
+++ deploy.php
@@ -4,7 +4,7 @@ namespace Deployer;
 require 'recipe/common.php';
 
 // Project name
-set('application', 'deployer');
+set('application', 'my_project');
 
 // Project repository
 set('repository', 'git@github.com:pyRadiohead/deployer-test.git');
@@ -12,7 +12,6 @@ set('repository', 'git@github.com:pyRadiohead/deployer-test.git');
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true);
 
-// Shared files/dirs between deploys
 set('shared_files', [
 	'wp-config.php',
 ]);
@@ -32,8 +31,10 @@ set('clear_paths', [
 	'.gitignore',
 	'deploy.php',
 ]);
+set('allow_anonymous_stats', false);
 
 // Hosts
+
 host('production')
 	->hostname('207.154.209.72')
 	->port(22)
@@ -41,29 +42,30 @@ host('production')
 	->identityFile('~/.ssh/id_rsa')
 	->set('deploy_path', '/var/www/deployer')
 	->set('keep_releases', 3);
+    
 
 // Tasks
+
 desc('Deploy your project');
 task('deploy', [
-	'deploy:info',
-	'deploy:prepare',
-	'deploy:lock',
-	'deploy:release',
-	'deploy:update_code',
-	'deploy:shared',
-	'deploy:writable',
-	'deploy:clear_paths',
-	'deploy:symlink',
-	'deploy:unlock',
-	'cleanup',
-	'success'
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:vendors',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
 ]);
-
-// Install WordPress.
+//Install wordpress
 desc('Install WordPress');
-task('deploy:wp', '
-	wp core download;
-');
+task('deploy:wp',
+'wp core download;');
 
 after('deploy:writable', 'deploy:wp');
 
